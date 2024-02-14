81370c15ce2da05d779ffd9d44db483ebfef2649
elijah <cuiyeqing960904@gmail.com>
2024-02-14 05:16:03 -0500

    user handler

--- api/user_handler.go
+++ api/user_handler.go
@@ -1,11 +1,29 @@
 package api
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/co-codin/hotel-reservation/db"
+	"github.com/gofiber/fiber/v2"
+)
 
-func HandleGetUsers(c *fiber.Ctx) error {
-	return c.JSON("test")
+type UserHandler struct {
+	userStore db.UserStore
 }
 
-func HandleGetUser(c *fiber.Ctx) error {
+func NewUserHandler(userStore db.UserStore) *UserHandler {
+	return &UserHandler{
+		userStore: userStore,
+	}
+}
+
+func (h *UserHandler) HandleGetUser(c *fiber.Ctx) error {
+	id := c.Params("id")
+	user, err := h.userStore.GetUserByID(id)
+	if err != nil {
+		return err
+	}
+	return c.JSON(user)
+}
+
+func (h *UserHandler) HandleGetUsers(c *fiber.Ctx) error {
 	return c.JSON("test")
 }
