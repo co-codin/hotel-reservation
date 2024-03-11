54cc298b9b8bbfae465b7382492b34533ac288cb
elijah <cuiyeqing960904@gmail.com>
2024-03-11 07:03:33 -0400

    db stores built

--- scripts/seed.go
+++ scripts/seed.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"math/rand"
 	"os"
+	"time"
 
 	"github.com/co-codin/hotel-reservation/db"
 	"github.com/joho/godotenv"
@@ -35,18 +38,18 @@ func main() {
 		Hotel:   hotelStore,
 	}
 
-	// user := fixtures.AddUser(store, "james", "foo", false)
-	// fmt.Println("james ->", api.CreateTokenFromUser(user))
-	// admin := fixtures.AddUser(store, "admin", "admin", true)
-	// fmt.Println("admin ->", api.CreateTokenFromUser(admin))
-	// hotel := fixtures.AddHotel(store, "some hotel", "bermuda", 5, nil)
-	// room := fixtures.AddRoom(store, "large", true, 88.44, hotel.ID)
-	// booking := fixtures.AddBooking(store, user.ID, room.ID, time.Now(), time.Now().AddDate(0, 0, 5))
-	// fmt.Println("booking ->", booking.ID)
+	user := fixtures.AddUser(store, "james", "foo", false)
+	fmt.Println("james ->", api.CreateTokenFromUser(user))
+	admin := fixtures.AddUser(store, "admin", "admin", true)
+	fmt.Println("admin ->", api.CreateTokenFromUser(admin))
+	hotel := fixtures.AddHotel(store, "some hotel", "bermuda", 5, nil)
+	room := fixtures.AddRoom(store, "large", true, 88.44, hotel.ID)
+	booking := fixtures.AddBooking(store, user.ID, room.ID, time.Now(), time.Now().AddDate(0, 0, 5))
+	fmt.Println("booking ->", booking.ID)
 
-	// for i := 0; i < 100; i++ {
-	// 	name := fmt.Sprintf("random hotel name %d", i)
-	// 	location := fmt.Sprintf("location %d", i)
-	// 	fixtures.AddHotel(store, name, location, rand.Intn(5)+1, nil)
-	// }
+	for i := 0; i < 100; i++ {
+		name := fmt.Sprintf("random hotel name %d", i)
+		location := fmt.Sprintf("location %d", i)
+		fixtures.AddHotel(store, name, location, rand.Intn(5)+1, nil)
+	}
 }
