34ac1734ff1ab08e5b6bdb0d131218e332d9fd4b
elijah <cuiyeqing960904@gmail.com>
2024-03-11 07:32:43 -0400

    auth built

--- api/util.go
+++ api/util.go
@@ -1,1 +1,16 @@
 package api
+
+import (
+	"fmt"
+
+	"github.com/co-codin/hotel-reservation/types"
+	"github.com/gofiber/fiber/v2"
+)
+
+func getAuthUser(c *fiber.Ctx) (*types.User, error) {
+	user, ok := c.Context().UserValue("user").(*types.User)
+	if !ok {
+		return nil, fmt.Errorf("unauthorized")
+	}
+	return user, nil
+}
--- db/user_store.go
+++ db/user_store.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"fmt"
 	"os"
 
 	"github.com/co-codin/hotel-reservation/types"
@@ -14,7 +15,14 @@ const userColl = "users"
 
 type Map map[string]any
 
+type Dropper interface {
+	Drop(context.Context) error
+}
+
 type UserStore interface {
+	Dropper
+
+	GetUserByEmail(context.Context, string) (*types.User, error)
 	GetUserByID(context.Context, string) (*types.User, error)
 	GetUsers(context.Context) ([]*types.User, error)
 	InsertUser(context.Context, *types.User) (*types.User, error)
@@ -35,52 +43,9 @@ func NewMongoUserStore(client *mongo.Client) *MongoUserStore {
 	}
 }
 
-func (s *MongoUserStore) GetUsers(ctx context.Context) ([]*types.User, error) {
-	cur, err := s.coll.Find(ctx, bson.M{})
-	if err != nil {
-		return nil, err
-	}
-	var users []*types.User
-	if err := cur.All(ctx, &users); err != nil {
-		return nil, err
-	}
-	return users, nil
-}
-
-func (s *MongoUserStore) GetUserByID(ctx context.Context, id string) (*types.User, error) {
-	oid, err := primitive.ObjectIDFromHex(id)
-	if err != nil {
-		return nil, err
-	}
-
-	var user types.User
-	if err := s.coll.FindOne(ctx, bson.M{"_id": oid}).Decode(&user); err != nil {
-		return nil, err
-	}
-	return &user, nil
-}
-
-func (s *MongoUserStore) InsertUser(ctx context.Context, user *types.User) (*types.User, error) {
-	res, err := s.coll.InsertOne(ctx, user)
-	if err != nil {
-		return nil, err
-	}
-	user.ID = res.InsertedID.(primitive.ObjectID)
-	return user, nil
-}
-
-func (s *MongoUserStore) DeleteUser(ctx context.Context, id string) error {
-	oid, err := primitive.ObjectIDFromHex(id)
-	if err != nil {
-		return err
-	}
-
-	_, err = s.coll.DeleteOne(ctx, bson.M{"_id": oid})
-	if err != nil {
-		return err
-	}
-
-	return nil
+func (s *MongoUserStore) Drop(ctx context.Context) error {
+	fmt.Println("--- dropping user collection")
+	return s.coll.Drop(ctx)
 }
 
 func (s *MongoUserStore) UpdateUser(ctx context.Context, filter Map, params types.UpdateUserParams) error {
@@ -95,4 +60,59 @@ func (s *MongoUserStore) UpdateUser(ctx context.Context, filter Map, params type
 		return err
 	}
 	return nil
+}
+
+func (s *MongoUserStore) DeleteUser(ctx context.Context, id string) error {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
+	// TODO: Maybe its a good idea to handle if we did not delete any user.
+	// maybe log it or something??
+	_, err = s.coll.DeleteOne(ctx, bson.M{"_id": oid})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *MongoUserStore) InsertUser(ctx context.Context, user *types.User) (*types.User, error) {
+	res, err := s.coll.InsertOne(ctx, user)
+	if err != nil {
+		return nil, err
+	}
+	user.ID = res.InsertedID.(primitive.ObjectID)
+	return user, nil
+}
+
+func (s *MongoUserStore) GetUsers(ctx context.Context) ([]*types.User, error) {
+	cur, err := s.coll.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	var users []*types.User
+	if err := cur.All(ctx, &users); err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+func (s *MongoUserStore) GetUserByEmail(ctx context.Context, email string) (*types.User, error) {
+	var user types.User
+	if err := s.coll.FindOne(ctx, bson.M{"email": email}).Decode(&user); err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (s *MongoUserStore) GetUserByID(ctx context.Context, id string) (*types.User, error) {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, err
+	}
+	var user types.User
+	if err := s.coll.FindOne(ctx, bson.M{"_id": oid}).Decode(&user); err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
